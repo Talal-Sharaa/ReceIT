4a62ea6a2aed02f8c304e1dc9a7d9608b00a1625
Talal Sharaa <talal.i.sharaa@gmail.com>
2025-10-25 17:54:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9529721215-5de1d",
+  "appId": "1:336744646095:web:4c81557bc6c913b8887eba",
+  "apiKey": "AIzaSyB60I3BYMyIu9LpwEDlQc2tFz8gHx1tr7k",
+  "authDomain": "studio-9529721215-5de1d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "336744646095"
+};
